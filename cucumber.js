2bc55d002ee36ee0cb7ed568e93aee3bc562c129
Brian Mackey <mackey.brian@gmail.com>
2022-11-02 14:56:29 -0500

    Rudimentary example of cucumber in angular

---
+++ cucumber.js
@@ -0,0 +1,8 @@
+module.exports = {
+    default: [
+      "--require-module ts-node/register",
+      "--require features/**/*.ts",
+      "--publish-quiet",
+    ].join(" "),
+  };
+  
---
+++ features/step_definitions/steps.ts
@@ -0,0 +1,22 @@
+//import assert from "assert";  //TODO: Discover why this fails
+const assert = require('assert')
+
+import { When, Then } from "@cucumber/cucumber";
+import { Greeter } from "../../src/greeter";
+
+interface MyWorld {
+  whatIHeard: string;
+}
+
+When("the greeter says hello", function (this: MyWorld) {
+  this.whatIHeard = new Greeter().sayHello();
+});
+
+Then(
+  "I should have heard {string}",
+  function (this: MyWorld, expectedResponse: string) {
+    console.log('this: ' + this);
+    console.log('expectedResponse: ' + expectedResponse);
+    assert.equal(this.whatIHeard, expectedResponse);
+  }
+);
