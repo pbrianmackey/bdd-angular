75d70e5fc43e2c4415766e7c4e5ba071ddacdc86
Brian Mackey <mackey.brian@gmail.com>
2022-11-02 20:38:02 -0500

    Fix unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('bdd-angular app is running!');
+    expect(compiled.querySelector('div')?.textContent).toContain('Hello!!');
   });
 });
